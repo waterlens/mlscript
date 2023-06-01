4f0d49b0c08dc9ee3024989d7140b7d8a8f81a0e
CrescentonC <43136427+CrescentonC@users.noreply.github.com>
2023-06-01 16:50:24 +0800

    haskell example use own map def

--- lumberhack-haskell-benchmark/mapmap2.hs
+++ lumberhack-haskell-benchmark/mapmap2.hs
@@ -8,16 +8,22 @@ map₁2 f¹ ls¹ f² = (case ls¹ of {(h⁰ : t⁰) -> (let h¹ = (f¹ h⁰) in 
 
 mapff f1 ls f2 = case ls of { (h:t) -> (f2 $ f1 h):(mapff f1 t f2); [] -> []}
 
+
+myMap f [] = []
+myMap f (h:t) = (f h):(myMap f t)
+
 testOur ls = ((map₀ (\x⁰ -> (x⁰ + 1))) ((map₁ (\x¹ -> (x¹ + x¹))) ls))
 testOurFloatOut ls = ((map₀ (\x⁰ -> (x⁰ + 1))) ((map₁2 (\x¹ -> (x¹ + x¹))) ls))
 testDefault ls = map (\x -> x + 1) $ map (\x -> x + x) ls
+testDefaultOurMap ls = myMap (\x -> x + 1) $ myMap (\x -> x + x) ls
 testManual ls = map (\x -> x + x + 1) ls
 testManual2 ls = mapff (\x -> x + x) ls (\x -> x + 1)
 
 main :: IO ()
 main = defaultMain [ bgroup "f1" [
-                                 bench "our" $ nf testOur [1..5000000]
+                                 bench "lumberhack" $ nf testOur [1..5000000]
                                  , bench "default" $ nf testDefault [1..5000000]
+                                 , bench "defaultOurMap" $ nf testDefaultOurMap [1..5000000]
                                  , bench "ourFloatOut" $ nf testOurFloatOut [1..5000000]
                                  , bench "manual2" $ nf testManual2 [1..5000000]
                                  , bench "manual" $ nf testManual [1..5000000] ] ]

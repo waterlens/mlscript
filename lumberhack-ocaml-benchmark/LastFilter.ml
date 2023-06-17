8eedcc4f00de483eee5ca5fe11560578c84b58ce
CrescentonC <43136427+CrescentonC@users.noreply.github.com>
2023-06-17 22:43:14 +0800

    update lastfilter manually fused program

--- lumberhack-ocaml-benchmark/LastFilter.ml
+++ lumberhack-ocaml-benchmark/LastFilter.ml
@@ -7,15 +7,15 @@ ocamlfind ocamlopt -rectypes -thread -O3 ./LastFilter.ml -o "./LastFilter.out" -
 open Core_bench;;
 
 (* original *)
-let rec _lhManualLastFilter ls_4 f_1 a_2 =
-  (match ls_4 with
-    | `C(h_3, t_3) -> 
-      (if (f_1 h_3) then
-        (((_lhManualLastFilter t_3) f_1) (`Some(h_3)))
+let rec _lhManualLastFilter ls_5 f_2 a_3 =
+  (match ls_5 with
+    | `C(h_4, t_4) -> 
+      (if (f_2 h_4) then
+        (((_lhManualLastFilter t_4) f_2) h_4)
       else
-        (((_lhManualLastFilter t_3) f_1) a_2))
+        (((_lhManualLastFilter t_4) f_2) a_3))
     | `N -> 
-      a_2);;
+      a_3);;
 let rec enumFromTo a_0 b_0 =
   (if (a_0 <= b_0) then
     (`C(a_0, ((enumFromTo (a_0 + 1)) b_0)))
@@ -36,7 +36,17 @@ let rec last a_1 ls_2 =
       a_1
     | `C(h_2, t_2) -> 
       ((last h_2) t_2));;
-let rec lastDrive ls_1 =
+let rec _lhManualLastFilterDrive ls_4 f_1 =
+  (match ls_4 with
+    | `C(h_3, t_3) -> 
+      (if (f_1 h_3) then
+        (let rec a_2 = (((_lhManualLastFilter t_3) f_1) h_3) in
+          (`Some(a_2)))
+      else
+        ((_lhManualLastFilterDrive t_3) f_1))
+    | `N -> 
+      (`None))
+and lastDrive ls_1 =
   (match ls_1 with
     | `C(h_1, t_1) -> 
       (`Some(((last h_1) t_1)))
@@ -45,34 +55,34 @@ let rec lastDrive ls_1 =
 and testLastFilter ls_3 =
   (lastDrive ((filter ls_3) (fun x_0 -> 
     (x_0 < 1000))))
-and testManual ls_5 =
-  (((_lhManualLastFilter ls_5) (fun x_1 -> 
-    (x_1 < 1000))) (`None));;
+and testManual ls_6 =
+  ((_lhManualLastFilterDrive ls_6) (fun x_1 -> 
+    (x_1 < 1000)));;
 
 (* lumberhack *)
-let rec enumFromTo_d0 a_4 b_0 =
-  (if (a_4 <= b_0) then
-    (`C(a_4, ((enumFromTo_d0 (a_4 + 1)) b_0)))
+let rec enumFromTo_d0 a_6 b_1 =
+  (if (a_6 <= b_1) then
+    (`C(a_6, ((enumFromTo_d0 (a_6 + 1)) b_1)))
   else
     (`N));;
-let rec enumFromTo_d1 a_5 b_1 =
-  (if (a_5 <= b_1) then
-    (`C(a_5, ((enumFromTo_d1 (a_5 + 1)) b_1)))
+let rec enumFromTo_d1 a_3 b_0 =
+  (if (a_3 <= b_0) then
+    (`C(a_3, ((enumFromTo_d1 (a_3 + 1)) b_0)))
   else
     (`N));;
 let rec lastDrive_d0 ls_0 =
   ls_0;;
 let rec last_d0 a_2 ls_2 =
   (ls_2 a_2);;
-let rec filter_d0 ls_4 f_1 =
-  (match ls_4 with
-    | `C(h_2, t_2) -> 
-      (if (f_1 h_2) then
-        (let rec h_3 = h_2 in
-          (let rec t_3 = ((filter_d1 t_2) f_1) in
-            (`Some(((last_d0 h_3) t_3)))))
+let rec filter_d0 ls_5 f_2 =
+  (match ls_5 with
+    | `C(h_3, t_3) -> 
+      (if (f_2 h_3) then
+        (let rec h_4 = h_3 in
+          (let rec t_4 = ((filter_d1 t_3) f_2) in
+            (`Some(((last_d0 h_4) t_4)))))
       else
-        ((filter_d0 t_2) f_1))
+        ((filter_d0 t_3) f_2))
     | `N -> 
       (`None))
 and filter_d1 ls_1 f_0 =
@@ -93,46 +103,46 @@ and testLastFilter_d0 ls_3 =
     (x_0 < 1000))));;
 
 (* lumberhack_pop_out *)
-let rec enumFromTo_d0_d0 a_1_0 b_3 =
-  (if (a_1_0 <= b_3) then
-    (`C(a_1_0, ((enumFromTo_d0_d0 (a_1_0 + 1)) b_3)))
+let rec enumFromTo_d0_d0 a_9 b_2 =
+  (if (a_9 <= b_2) then
+    (`C(a_9, ((enumFromTo_d0_d0 (a_9 + 1)) b_2)))
   else
     (`N));;
-let rec enumFromTo_d1_d0 a_8 b_2 =
-  (if (a_8 <= b_2) then
-    (`C(a_8, ((enumFromTo_d1_d0 (a_8 + 1)) b_2)))
+let rec enumFromTo_d1_d0 a_1_4 b_3 =
+  (if (a_1_4 <= b_3) then
+    (`C(a_1_4, ((enumFromTo_d1_d0 (a_1_4 + 1)) b_3)))
   else
     (`N));;
-let rec lastDrive_d0_d0 ls_1_5 =
-  ls_1_5;;
-let rec last_d0_d0 a_7 ls_9 =
-  (ls_9 a_7);;
-let rec last_d0_d1 a_1_1 ls_1_1 =
-  (ls_1_1 a_1_1);;
-let rec filter_d0_d0 ls_1_2 f_5 =
+let rec lastDrive_d0_d0 ls_1_6 =
+  ls_1_6;;
+let rec last_d0_d0 a_1_2 ls_1_4 =
+  (ls_1_4 a_1_2);;
+let rec last_d0_d1 a_1_0 ls_1_0 =
+  (ls_1_0 a_1_0);;
+let rec filter_d0_d0 ls_1_2 f_6 =
   (match ls_1_2 with
-    | `C(h_7, t_7) -> 
-      (if (f_5 h_7) then
-        (let rec h_8 = h_7 in
-          (let rec t_8 = ((filter_d1_d0 t_7) f_5) in
-            (`Some(((last_d0_d1 h_8) t_8)))))
-      else
-        ((filter_d0_d0 t_7) f_5))
-    | `N -> 
-      (`None))
-and filter_d1_d0 ls_1_4 f_6 =
-  (match ls_1_4 with
     | `C(h_9, t_9) -> 
       (if (f_6 h_9) then
         (let rec h_1_0 = h_9 in
           (let rec t_1_0 = ((filter_d1_d0 t_9) f_6) in
-            (fun a_1_2 -> 
-              ((last_d0_d0 h_1_0) t_1_0))))
+            (`Some(((last_d0_d1 h_1_0) t_1_0)))))
       else
-        ((filter_d1_d0 t_9) f_6))
+        ((filter_d0_d0 t_9) f_6))
     | `N -> 
-      (fun a_1_3 -> 
-        a_1_3))
+      (`None))
+and filter_d1_d0 ls_9 f_4 =
+  (match ls_9 with
+    | `C(h_6, t_6) -> 
+      (if (f_4 h_6) then
+        (let rec h_7 = h_6 in
+          (let rec t_7 = ((filter_d1_d0 t_6) f_4) in
+            (fun a_7 -> 
+              ((last_d0_d0 h_7) t_7))))
+      else
+        ((filter_d1_d0 t_6) f_4))
+    | `N -> 
+      (fun a_8 -> 
+        a_8))
 and testLastFilter_d0_d0 ls_8 =
   (lastDrive_d0_d0 ((filter_d0_d0 ls_8) (fun x_2 -> 
     (x_2 < 1000))));;
